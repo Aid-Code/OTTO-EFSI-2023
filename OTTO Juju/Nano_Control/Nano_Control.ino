c6ca7f1a40cc61def7f715bd65ac37c5e1b1d687
juliv07 <julivinocur@gmail.com>
2023-09-06 15:17:36 -0300

    Subí Nano Control

---
+++ OTTO Juju/Nano_Control/Nano_Control.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+#include <SPI.h>
+#include <RF24.h>
+
+#define VRX A5    // Arduino pin connected to VRX pin
+#define VRY A4  // Arduino pin connected to Servo motor 1
+
+int i = 2000;
+int s = 3000;
+int x;
+int y;
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte identificacion[6] = "00001";
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(VRX, INPUT);
+  pinMode(VRY, INPUT);
+
+  radio.begin();
+  radio.openWritingPipe(identificacion);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+
+}
+
+void loop() {
+  
+  x = analogRead(VRX);
+  y = analogRead(VRY);
+
+  radio.write(&i, sizeof(i));
+  radio.write(&x, sizeof(x));
+  radio.write(&s, sizeof(s));
+  radio.write(&y, sizeof(y));
+
+}
